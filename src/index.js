990d7e5d9b2dfb346471ab74906d8ed25098736e
dimitos <kurort26@mail.ru>
2021-07-10 21:20:20 +0300

    feat: add function content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.replace(/ /g, ''); // remove all spaces
+  if (expr.indexOf('/0') !== -1)
+    // if the line contains division by 0, then throw an 'TypeError'
+    throw new Error('TypeError: Division by zero.');
+
+  // count open and closed parentheses
+  let brackets = 0;
+  expr.split('').forEach((el) => {
+    el === '(' ? brackets++ : false;
+    el === ')' ? brackets-- : false;
+  });
+
+  if (brackets !== 0)
+    // if there are unpaired parentheses, then throw an'TypeError'
+    throw new Error('ExpressionError: Brackets must be paired');
+
+  // insert string expression into function body
+  expr = new Function('return ' + expr);
+
+  // run new Function and return value
+  return expr();
 }
 
 module.exports = {
